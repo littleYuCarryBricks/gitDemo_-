d4230efbe5a31b3eba22a7f716756687a2b47642
lenovo <1325966119@qq.com>
2021-10-02 22:00:39 +0800

    9 try

--- src/cn/edu/whut/Hello_Git.java
+++ src/cn/edu/whut/Hello_Git.java
@@ -3,6 +3,5 @@ package cn.edu.whut;
 public class Hello_Git {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hhhhh");
     }
 }
--- src/cn/edu/whut/gitDemo_1.java
+++ src/cn/edu/whut/gitDemo_1.java
@@ -4,11 +4,7 @@ package cn.edu.whut;
 public class gitDemo_1 {
     public static void main(String[] args) {
         int a = 0;
-        int b = 1;
-        int c = 1;
-        int d = 3;
-        int e = 1;
-        int q = 0;
+
         System.out.println("hello");
 
     }
